640f28b0c4df9b88e408cb4ac25acd865007339e
voyagerlin <voyagerlin@gmail.com>
2017-07-31 07:36:57 +0800

    first commit

---
+++ WebApiTest/Controllers/FileUploadController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Threading.Tasks;
+using System.IO; 
+
+namespace WebApiTest.Controllers
+{
+    //https://github.com/damienbod/WebApiFileUpload
+    public class FileUploadController : ApiController
+    {
+        private static readonly string FileLocation = "e:\\tools";
+
+        [Route("files")]
+        [HttpPost]
+        [MultipartContentFilter]
+        public async Task<FileResult> UploadFiles()
+        {
+            var streamProvider = new MultipartFormDataStreamProvider(FileLocation);
+            await Request.Content.ReadAsMultipartAsync(streamProvider);
+
+            var result = new FileResult
+            {
+                FileNames = streamProvider.FileData.Select(c => c.LocalFileName),
+                Names = streamProvider.FileData.Select(c => c.Headers.ContentDisposition.FileName),
+                ContentTypes = streamProvider.FileData.Select(c => c.Headers.ContentType.MediaType),
+                Description = streamProvider.FormData["description"],
+                CreatedTimestamp = DateTime.Now,
+                UpdatedTimestamp = DateTime.Now,
+                DownloadLink = "we will tell you"
+
+            };
+
+            return result;
+            //return new FileResult
+            //{
+            //    FileNames = streamProvider.FileData.Select(c => c.LocalFileName),
+            //    Names = streamProvider.FileData.Select(c=> c.Headers.ContentDisposition.FileName),
+            //    ContentTypes = streamProvider.FileData.Select(c=> c.Headers.ContentType.MediaType),
+            //    Description = streamProvider.FormData["description"],
+            //    CreatedTimestamp = DateTime.Now,
+            //    UpdatedTimestamp = DateTime.Now,
+            //    DownloadLink = "we will tell you"
+
+            //};
+        }
+    }
+
+
+}
---
+++ WebApiTest/Controllers/MultipartContentFilter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace WebApiTest.Controllers
+{
+    public class MultipartContentFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            base.OnActionExecuting(actionContext);
+
+            if (!actionContext.Request.Content.IsMimeMultipartContent())
+            {
+                throw new HttpResponseException(System.Net.HttpStatusCode.UnsupportedMediaType);
+            }
+        }
+    }
+}
